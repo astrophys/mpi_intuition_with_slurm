657cfdd1de8dd911f40a36836a41d2be237a2682
Ali Snedden <ali.snedden@nationwidechildrens.org>
2020-04-14 10:31:35 -0400

    Created function compute_integral() to cleanly integrate on individual tasks

--- mpi_integrate.c
+++ mpi_integrate.c
@@ -22,53 +22,83 @@ double y(double x)
     return x*x;
 }
 
+/*
+void print_message(char * str, int verbose)
+{
+    if(verbose == 1){
+        printf("%s",str)
+    }else{
+        printf("%s",str)
+    }
+}
+*/
+
+double compute_integral(int xmin, int xmax, int ntasks, int taskID)
+{
+    /* Get each Task's Range.*/
+    double task_xmin = taskID * (xmax - xmin) / ntasks;
+    double task_xmax = (taskID + 1) * (xmax - xmin) / ntasks;
+    double dx   = 0.00001;
+    double task_result = 0;
+    double i=0;
+    printf("Task %i : [%.3f,%.3f]\n", taskID, task_xmin, task_xmax);
+    fflush(stdout);
+    
+    
+    /* Break up integration*/ 
+    for(i=task_xmin; i<task_xmax; i+=dx)
+    {
+        // Trapezoidal rule
+        task_result += (y(i+dx) - y(i)) * dx / 2.0 +  y(i) * dx;
+    }
+    return(task_result);
+}
+
 
 int main(int argc, char * argv[])
 {
     int taskID = -1;
-    int NTasks = -1;
-    /*double i    =  0;
-    double xmax = 10;
+    int ntasks = -1;
+    int nSteps = 100;
+    int step = 0;
+    int verbose = 0;
+    double xmax = 1000;
     double xmin =  0;
-    double dx   = 0.00001;
     double task_xmax = 0;
     double task_xmin = 0;
     double task_result = 0;
     double glob_result = 0;
-    */
-
-
 
     /* MPI Initializations */
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &taskID);
-    MPI_Comm_size(MPI_COMM_WORLD, &NTasks);
-
-
-   
-    /* Get each Task's Range.*/
-    //task_xmin = taskID * (xmax - xmin) / NTasks;
-    //task_xmax = (taskID + 1) * (xmax - xmin) / NTasks;
-
-
-    /* Break up integration*/ 
-    /*for(i=task_xmin; i<task_xmax; i+=dx)
-    {
-        task_result += (y(i+dx) - y(i)) * dx / 2.0 +  y(i) * dx;
-    }*/
-  
-
-    /* Collect Results */
-    //MPI_Reduce(&task_result, &glob_result, 1, MPI_DOUBLE, MPI_SUM, 0, 
-    //           MPI_COMM_WORLD);
-
-
-    
+    MPI_Comm_size(MPI_COMM_WORLD, &ntasks);
     printf("Hello World from Task %i\n", taskID);
-    /*if(taskID == 0)
-    {
-        printf("Integration from %f to %f of x2 = %f\n", xmin, xmax, glob_result);
-    }*/
+
+
+    /* Master Loop */ 
+    for(step=0; step<nSteps; step++){
+        if(taskID == 0){
+            //printf("Step : %i\n", step);
+            task_result = compute_integral(xmin, xmax, ntasks, taskID);
+
+        }else{
+            task_result = compute_integral(xmin, xmax, ntasks, taskID);
+        }
+        MPI_Barrier(MPI_COMM_WORLD);    // Ensure every task completes
+
+        /* Collect Results */
+        MPI_Reduce(&task_result, &glob_result, 1, MPI_DOUBLE, MPI_SUM, 0, 
+                   MPI_COMM_WORLD);
+
+        if(taskID == 0)
+        {
+            printf("Step %i Integration from %f to %f of x2 = %f\n", step, xmin,
+                   xmax, glob_result);
+            fflush(stdout);
+        }
+        glob_result=0;
+    }
 
     MPI_Finalize();
     return 0;
